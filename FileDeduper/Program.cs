1576fe4cff233258589121e75775b15839f3cada
Dominic Hopton <d.hopton@codevoid.net>
2016-12-27 15:19:29 -0800

    Updating to CLR v4.6.2 Updating Default Namespace Adding Empty output

--- FileDeduper/Program.cs
+++ FileDeduper/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileDeduper
+namespace Codevoid.Utility.FileDeduper
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("FileDeduper -- Scans a file tree and lists any idenitical files");
+            Console.WriteLine("Copyright 2016, Dominic Hopton");
         }
     }
 }
